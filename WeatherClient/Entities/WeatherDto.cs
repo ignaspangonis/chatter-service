a84b00804c7c17ff7554e2ac3534f516de3045af
ignaspangonis <ignas.pangonis@vinted.com>
2024-10-28 22:48:51 +0200

    Add WeatherClient

---
+++ WeatherClient/Entities/WeatherDto.cs
@@ -0,0 +1,30 @@
+﻿namespace WeatherClient.Entities
+{
+    public interface IWeatherDto
+    {
+        public double Temperature { get; set; }
+        public string Time { get; set; }
+        public string Summary { get; set; }
+    }
+
+    public class WeatherDto
+    {
+        public WeatherDto()
+        {
+            Time = "";
+            Summary = "";
+        }
+
+        public WeatherDto(double temperature, string time, string summary)
+        {
+            Temperature = temperature;
+            Time = time;
+            Summary = summary;
+        }
+
+        public double Temperature { get; set; }
+        public string Time { get; set; }
+        public string Summary { get; set; }
+    }
+}
+
